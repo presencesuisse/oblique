4c5277872bfe9b436babd2923134522385009865
presencesuisse <presence.suisse@gmail.com>
2025-08-22 17:52:34 +0200

    Publish application

--- projects/design-system/src/lib/demo.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-	selector: 'ob-design-system',
-	standalone: true,
-	templateUrl: './demo.component.html'
-})
-export class DemoComponent {}
